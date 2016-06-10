c893fc4139366cf6005b9bee9caee56f03c33816
SpectrumRead <SpectrumRead>
2016-06-10 14:33:47 +0200

    add some pieces

--- chess3/IPiece.cs
+++ chess3/IPiece.cs
@@ -8,5 +8,70 @@ namespace chess3
 {
     interface IPiece
     {
+        void movePiece();
+
+    }
+
+    class Board
+    {
+        IPiece [][];
+    }
+
+    class Pawn
+    {
+        int x;
+        int y;
+        char color;
+        void movePiece(Pawn pawn, int x, int y)
+        {
+
+
+        }
+
+    }
+    class Knight
+    {
+        int x;
+        int y;
+        char color;
+        void movePiece(Rook rook, int x, int y)
+        {
+
+        }
+
+    }
+    class Bishop
+    {
+        int x;
+        int y;
+        char color;
+        void movePiece(Bishop bishop, int x, int y)
+        {
+
+        }
+
+    }
+    class Rook
+    {
+        int x;
+        int y;
+        char color;
+        void movePiece(Rook rook, int x, int y)
+        {
+
+        }
+
+    }
+    
+    class Queen
+    {
+        int x;
+        int y;
+        char color;
+        void movePiece(Queen rook, int x, int y)
+        {
+
+        }
+
     }
 }
